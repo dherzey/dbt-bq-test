23504fa892a831af04e843898a558d661341be23
Jersey Dianne <jdtganding0998@outlook.com>
2024-10-28 15:51:30 +0800

    add stg_student_w_id

---
+++ models/staging/stg_student_w_id.sql
@@ -0,0 +1,11 @@
+{{ config(materialized="table") }}
+
+select 
+  left(sha1(concat(
+    Attendance,
+    Parental_Involvement,
+    Exam_Score,
+    Peer_Influence
+  )),8) as id,
+  *
+from `sample.student_performance`
